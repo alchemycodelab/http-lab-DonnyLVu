47b21a1408b4896672c3e011304293a59242b315
DonnyLVu <donnyvu897@gmail.com>
2020-12-14 16:21:09 -0800

    add better parseRequest

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,22 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it('this should GET hi', async () => {
+    const res = await request(app)
+      .get('/');
 
+    expect(res.text).toEqual('hi');
+  });
+
+  it('post status code 200 and plain text with the request body', async () => {
+    const res = await request(app)
+      .post('/echo')
+      .send('hi');
+
+    expect(res.text).toEqual({
+      body: 'hi',
+      status: '200 ok',
+      contentType: 'text/plain'
+    });
+  });
 });
